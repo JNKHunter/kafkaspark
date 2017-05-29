bf54ffa8164345ed2220985fe6b99380345a1509
John Hunter <john@pushagency.io>
2017-05-29 12:01:47 -0400

    added params

--- src/main/scala/StreamReader.scala
+++ src/main/scala/StreamReader.scala
@@ -7,16 +7,10 @@ class StreamReader {
 
   val kafkaParams = Map[String, Object](
     "bootstrap.servers" -> "172.31.74.41:9092",
-    
-  )
-
-
-  val kafkaParams = Map[String, Object](
-    "bootstrap.servers" -> "localhost:9092,anotherhost:9092",
     "key.deserializer" -> classOf[StringDeserializer],
     "value.deserializer" -> classOf[StringDeserializer],
-    "group.id" -> "use_a_separate_group_id_for_each_stream",
-    "auto.offset.reset" -> "latest",
+    "group.id" -> "group_id_0",
+    "audo.offset.reset" -> "latest",
     "enable.auto.commit" -> (false: java.lang.Boolean)
   )
 
