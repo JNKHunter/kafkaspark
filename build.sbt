87ca08d360ca8d36fa40cd14bd3016eeb7d03d1b
John Hunter <john@pushagency.io>
2017-05-29 12:35:57 -0400

    added naive implementation

--- build.sbt
+++ build.sbt
@@ -7,5 +7,4 @@ scalaVersion := "2.11.11"
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.1"
 libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.1.1"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.1.1"
-libraryDependencies += "org.apache.spark" % "spark-streaming-kafka_2.11" % "1.6.3"
-
+libraryDependencies += "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % "2.1.0"
--- src/main/scala/StreamReader.scala
+++ src/main/scala/StreamReader.scala
@@ -1,9 +1,16 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka010.KafkaUtils
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 
 /**
   * Created by John on 5/29/17.
   */
 class StreamReader {
+  val sparkConf = new SparkConf().setAppName("Log Reader")
+  val ssc = new StreamingContext(sparkConf, Seconds(2))
 
   val kafkaParams = Map[String, Object](
     "bootstrap.servers" -> "172.31.74.41:9092",
@@ -14,4 +21,13 @@ class StreamReader {
     "enable.auto.commit" -> (false: java.lang.Boolean)
   )
 
+  val topics = Array("topicA", "topicB")
+  val stream = KafkaUtils.createDirectStream[String, String](
+    ssc,
+    PreferConsistent,
+    Subscribe[String, String](topics, kafkaParams)
+  )
+
+  var streamMap = stream.map(record => (record.key, record.value))
+  streamMap.print
 }
